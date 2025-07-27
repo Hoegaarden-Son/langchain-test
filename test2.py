05f65d9349a00b849e7194c67f243498d9524e92
Hoegaarden-Son <hellopangx2@gmail.com>
2025-07-27 20:04:45 +0900

    streamlit code moved to main.py

--- test2.py
+++ test2.py
@@ -1,18 +1,81 @@
 from dotenv import load_dotenv
 load_dotenv()
-import streamlit as st
+
 from langchain_openai import ChatOpenAI
 
-chat_model = ChatOpenAI()
+import getpass
+import os
 
-st.title("인공지능 시인")
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
 
-content = st.text_input("시의 주제를 제시해주세요.")
 
-if st.button("시 작성 요청하기"):
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
 
-    with st.spinner("시 작성 중...", show_time=False):
-        result = chat_model.predict(content + "에 대한 시를 써줘.")
-        st.write(result)
-        
-    
+
+def chat_test():
+    llm = ChatOpenAI(
+        model="gpt-4o",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        # api_key="...",  # if you prefer to pass api key in directly instaed of using env vars
+        # base_url="...",
+        # organization="...",
+        # other params...
+    )
+
+    messages = [
+        (
+            "system",
+            "You are a helpful assistant that translates English to French. Translate the user sentence.",
+        ),
+        ("human", "I love programming."),
+    ]
+    ai_msg = llm.invoke(messages)
+    print(ai_msg)
+
+
+
+class Classification(BaseModel):
+    sentiment: str = Field(description="The sentiment of the text")
+    aggressiveness: int = Field(
+        description="How aggressive the text is on a scale from 1 to 10"
+    )
+    language: str = Field(description="The language the text is written in")
+
+
+def classify_text(text: str) -> Classification:
+    """
+    Classify the text into sentiment, aggressiveness, and language.
+    """
+    llm = init_chat_model("gpt-4o", model_provider="openai")
+
+    tagging_prompt = ChatPromptTemplate.from_template(
+        """
+    Extract the desired information from the following passage.
+
+    Only extract the properties mentioned in the 'Classification' function.
+
+    Passage:
+    {input}
+    """
+    )
+
+
+    # Structured LLM
+    structured_llm = llm.with_structured_output(Classification)
+
+
+    inp = "너 비트코인 좀 알아?"
+    prompt = tagging_prompt.invoke({"input": inp})
+    response = structured_llm.invoke(prompt)
+
+    print(response)
+    return response
+
+classify_text("")
